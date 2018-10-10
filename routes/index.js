8abc176e711cacd8976aea6487eda8d20d221b7c
Zita <atembo@bluecodeltd.com>
2018-10-11 00:41:21 +0200

    add sub url

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,17 @@ import express from 'express';
 let router = express.Router();
 
 import { CONTROLLERS } from "../controllers/controllers";
+const SUB_Url = "middleware/rest";
 
 CONTROLLERS.forEach( controller => {
 
-    router.get(`/${controller.resourceRoute}/`, controller.getAllResources);
+    router.get(`/${SUB_Url}/${controller.resourceRoute}/`, controller.getAllResources);
 
-    router.get(`^/${controller.resourceRoute}/:uuid([0-9a-fA-F]{8}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{12})`, controller.getResourcesByUUID);
+    router.get(`^/${SUB_Url}/${controller.resourceRoute}/:uuid([0-9a-fA-F]{8}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{12})`, controller.getResourcesByUUID);
 
-    router.get(`^/${controller.resourceRoute}/:uuid([0-9A]{36})`, controller.getResourcesByUUID);
+    router.get(`^/${SUB_Url}/${controller.resourceRoute}/:uuid([0-9A]{36})`, controller.getResourcesByUUID);
 
-    router.get(`^/${controller.resourceRoute}/:datetime(((-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(.[0-9]+)?(Z)?$))`, controller.getResourcesByDatetimeNewerThan);
+    router.get(`^/${SUB_Url}/${controller.resourceRoute}/:datetime(((-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(.[0-9]+)?(Z)?$))`, controller.getResourcesByDatetimeNewerThan);
 
 });
 
