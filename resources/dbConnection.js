b46c679bb6375d3e60863bff27e5b45284b7be37
Zita <atembo@bluecodeltd.com>
2020-01-27 16:30:19 +0200

    Disable timezone auto conversion

--- resources/dbConnection.js
+++ resources/dbConnection.js
@@ -5,7 +5,13 @@ const pool = mysql.createPool({
   host: process.env.MYSQL_HOST || "localhost", 
   user: process.env.MYSQL_USERNAME || "openmrs", 
   password: process.env.MYSQL_USER_PASSWORD || "openmrs", 
-  database: process.env.OPENMRS_MYSQL_DATABASE || "openmrs"
+  database: process.env.OPENMRS_MYSQL_DATABASE || "openmrs",
+  typeCast(field, next) {
+    if(field.type == 'DATETIME') {
+        return new Date(field.string() + 'Z');
+    }
+    return next();
+  }
 });
 
 export default pool;
